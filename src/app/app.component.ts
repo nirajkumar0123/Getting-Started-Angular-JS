94b92ee01e51190833b4d319b6d40f12f8ddad8c
Niraj Kumar <nirajgupta7481@gmail.com>
2025-03-05 23:25:08 +0530

    [Niraj] Add . fixed styling and structure for user input validation and improved ui consistency

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'HelloBridgeLabz';
+  title: string = "Hello from BridgeLabz";
+  imageUrl: string = 'assets/logo.jpg';
+  url: string = 'https://www.bridgelabz.com';
+  userName: string = '';
+  nameError: string = '';
+
+  validateUserName(): void {
+    const namePattern = /^[A-Z][a-zA-Z]{2,}$/;
+    this.nameError = namePattern.test(this.userName) ? '' : 'Incorrect Name!';
+  }
+
+  onClick(event: Event): void {
+    console.log("Save button is clicked", event);
+    window.open(this.url, "_blank");
+  }
+
+  onInput(event: Event): void {
+    console.log("Change Event Occurred", event);
+    this.validateUserName();
+  }
 }
